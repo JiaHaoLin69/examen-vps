c478f1c6f67dcd74138569fc32bcc0323828ead5
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-17 12:53:09 +0100

    a

--- docker/caronteweb/dockerfiles/autocaravaneando/admin/jhlwstart.sh
+++ docker/caronteweb/dockerfiles/autocaravaneando/admin/jhlwstart.sh
@@ -31,6 +31,7 @@ dependencias-y-servicio(){
     # Verificar si package.json existe
     if [ -f package.json ]; then
         npm install && echo "Dependencias instaladas" >> /root/logs/informe_react.log
+        npm audit fix --force && echo "Forzando auditoría de dependencias" >> /root/logs/informe_react.log
         # Inciar el servidor de desarrollo de React
         npm run dev -- --host 0.0.0.0 --port 3000 && echo "Servidor React iniciado" >> /root/logs/informe_react.log
     else
