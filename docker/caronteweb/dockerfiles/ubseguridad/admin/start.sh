6c9e7fe6a32d946b7b02adbc478bb2cad8afe215
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-17 11:44:31 +0100

    a

--- docker/caronteweb/dockerfiles/ubseguridad/admin/start.sh
+++ docker/caronteweb/dockerfiles/ubseguridad/admin/start.sh
@@ -6,7 +6,7 @@ load_entrypoint_base(){
 
 jambload_ciber(){
     LOG_DIR="/root/logs"
-    LOG_FILE="$LOG_DIR/ctjhlwautocaravaneando_ports.log"
+    LOG_FILE="/root/logs/ctjhlwautocaravaneando_ports.log"
     
     echo "=== PORT AUDITORIO ===" >> "$LOG_FILE"
     echo "Container: ${CONTENEDOR}" >> "$LOG_FILE"
